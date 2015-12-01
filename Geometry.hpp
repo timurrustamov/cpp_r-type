8dc9863b5dd442d3ca8b92fb4fb4c2b8d866b94b
twYnn <faby.borel@gmail.com>
2015-12-02 00:36:35 +0100

    wincompatibility

--- Geometry.hpp
+++ Geometry.hpp
@@ -3,9 +3,12 @@
 //
 
 #ifndef CPP_R_TYPE_GEOMETRY_HPP
-#define CPP_R_TYPE_GEOMETRY_HPP
+# define CPP_R_TYPE_GEOMETRY_HPP
 
-#include "t2Vector.hpp"
+# include "t2Vector.hpp"
+# ifndef M_PI
+#  define M_PI 3.1415926535897932384626433832795
+# endif
 
 class Geometry {
 
--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <unistd.h>
 #include "t2Vector.hpp"
 #include "Geometry.hpp"
 #include <SFML/Graphics.hpp>
