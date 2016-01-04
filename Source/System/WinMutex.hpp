45127741492fbf09958e573439ad40bd4ab7d4ae
twYnn <fabY.borel@gmail.com>
2016-01-04 12:08:22 +0100

    winMutex v2

--- Source/System/WinMutex.hpp
+++ Source/System/WinMutex.hpp
@@ -12,10 +12,14 @@ class WinMutex :		public IMutex
 {
 private:
 	HANDLE				_mutex;
+	unsigned int		_locknb;
+	DWORD WINAPI		_tid;
 
 public:
 	WinMutex()
 	{
+		this->_locknb = 0;
+		this->_tid = -1;
 		this->_mutex = CreateMutex(NULL, FALSE, NULL);
 	}
 
@@ -27,17 +31,29 @@ public:
 	virtual bool		lock(bool wait = true)
 	{
 		DWORD			waitResult;
+		bool			sameThread = (GetCurrentThreadId() == this->_tid);
 
-		if ((waitResult = WaitForSingleObject(this->_mutex, (wait) ? INFINITE : 0)) == WAIT_OBJECT_0)
+		if (this->_tid == -1 || !sameThread)
 		{
-			this->_status = IMutex::Locked;
-			return (true);
+			if ((waitResult = WaitForSingleObject(this->_mutex, (wait) ? INFINITE : 0)) == WAIT_OBJECT_0)
+			{
+				this->_status = IMutex::Locked;
+				return (true);
+			}
 		}
+		if (sameThread)
+			++this->_locknb;
 		return (false);
 	}
 
 	virtual bool		unlock()
 	{
+		bool			sameThread = (GetCurrentThreadId() == this->_tid);
+
+		if (this->_tid == -1 || (sameThread && --this->_locknb > 0))
+			return (false);
+
+		this->_tid = -1;
 		if (this->_status == IMutex::Locked)
 		{
 			ReleaseMutex(this->_mutex);
